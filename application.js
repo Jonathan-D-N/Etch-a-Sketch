c86ff6a9a09fc9912cb5ee25419a801ea903baa9
Jonathan <jdneave@gmail.com>
2022-12-05 16:36:58 -0700

    added removeElementByClass function

--- application.js
+++ application.js
@@ -1,12 +1,3 @@
-let slider = document.getElementById("gridRange");
-let output = document.getElementById("gridOutput");
-output.innerHTML = slider.value;
-
-slider.oninput = function () {
-    output.innerHTML = this.value;
-    console.log(this.value);
-    return(makeRows(this.value, this.value))
-} 
 
 //Create the grid
 const container = document.querySelector('#container')
@@ -23,10 +14,28 @@ function makeRows(rows, cols) {
     grid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
+        //cell.innerText = (c + 1);
         grid.appendChild(cell).className = "grid-item";
     };
 };
 
+makeRows(16, 16);
+//Remove the whole grid
+function removeElementByClass(className) {
+    const element = document.getElementsByClassName('grid-item');
+    while (element.length > 0) {
+        element[0].parentNode.removeChild(element[0]);
+    }
+}
 
+let slider = document.getElementById("gridRange");
+let output = document.getElementById("gridOutput");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+    console.log(this.value);
+    removeElementByClass('grid-item')
+    return(makeRows(this.value, this.value))
+} 
 
