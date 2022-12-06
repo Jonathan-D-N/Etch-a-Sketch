d39acde49dcf98f5e910fa1a2a5de47d7ea63157
Jonathan <jdneave@gmail.com>
2022-12-05 18:25:41 -0700

    adjust slider func to onchange. add oninput user feedback

--- application.js
+++ application.js
@@ -1,25 +1,20 @@
 
-//Create the grid
+//Create the gridContainer on page load
 const container = document.querySelector('#container')
 
 const grid = document.createElement('div');
-grid.classList.add('grid');
-//Assigns grid as the child of container
+grid.classList.add('gridContainer');
 container.appendChild(grid);
-
-//const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        //cell.innerText = (c + 1);
         grid.appendChild(cell).className = "grid-item";
     };
-};
+}; makeRows(16, 16);
 
-makeRows(16, 16);
 //Remove the whole grid
 function removeElementByClass(className) {
     const element = document.getElementsByClassName('grid-item');
@@ -27,15 +22,26 @@ function removeElementByClass(className) {
         element[0].parentNode.removeChild(element[0]);
     }
 }
-
+// Range Slider
 let slider = document.getElementById("gridRange");
 let output = document.getElementById("gridOutput");
-output.innerHTML = slider.value;
+let sliderValue = (0);
+output.innerHTML = slider.value + ' x ' + slider.value;
 
-slider.oninput = function () {
-    output.innerHTML = this.value;
-    console.log(this.value);
+slider.onchange = function () {
+    output.innerHTML = this.value + ' x ' + this.value;
+    sliderValue = this.value;
     removeElementByClass('grid-item')
     return(makeRows(this.value, this.value))
 } 
 
+slider.oninput = function () {
+    output.innerHTML = this.value + ' x ' + this.value;
+    sliderValue = this.value;
+}
+
+//const btn = document.querySelector('#btnGenerate');
+//btn.addEventListener('click', function(e) {
+//    removeElementByClass('grid-item');
+//    makeRows(sliderValue, sliderValue)
+//})
